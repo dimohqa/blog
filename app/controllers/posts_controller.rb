830edf3b8ebbc9d559e0a038149ce0cc30e67b81
dimohqa99 <3383015-dimohqa99@users.noreply.gitlab.com>
2020-12-23 18:50:43 +0700

    Add edit and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,28 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     # render plain: params[:post].inspect
     @post = Post.new(post_params)
