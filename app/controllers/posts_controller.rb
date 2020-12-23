50aeff6c192ff1a973854c4c4a7c8b7fc9d34663
dimohqa99 <3383015-dimohqa99@users.noreply.gitlab.com>
2020-12-23 17:52:37 +0700

    create post, show post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,25 @@ class PostsController < ApplicationController
   def index
 
   end
+
+  def new
+
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    @post = Post.new(post_params)
+
+    @post.save
+
+    redirect_to @post
+  end
+
+  private def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
