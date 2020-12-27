3d2c9e8c1617a2fe1176be29df8a400318984760
Stanislav <stolbennikovilmk@gmail.com>
2020-12-28 02:29:27 +0700

    Rewrite

--- spec/requests/comments_request_spec.rb
+++ spec/requests/comments_request_spec.rb
@@ -1,17 +1,4 @@
-#require 'rails_helper'
+require 'rails_helper'
 
-new_comment_params = {
-  author_id: '1',
-  username: 'Иван Иваныч',
-  body: 'new body'
-}
-
-RSpec.describe 'Comments', type: :request do
-  describe 'GET /index' do
-    it 'renders a successful response without id post' do
-      comment = Comment.create!(author_id: '1', username: 'Иван Иваныч', body: 'new body')
-      get post_comments_url
-      expect(response).to be_successful
-    end
-  end
-end
+#RSpec.describe 'Comments', type: :request do
+#end
