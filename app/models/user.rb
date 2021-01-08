14602db79963764426412ac28b06f0185916e707
Stanislav <stolbennikovilmk@gmail.com>
2021-01-09 01:51:35 +0700

    Fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   enum role: { admin: 0, user: 1, moderator: 2 }
 
-  has_many :posts
-  has_many :drafts
+  has_many :posts, dependent: :destroy
+  has_many :drafts, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
---
+++ db/migrate/20210108184806_add_depended_options.rb
@@ -0,0 +1,4 @@
+class AddDependedOptions < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
